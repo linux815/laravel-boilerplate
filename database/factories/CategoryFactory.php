fd24966a212c796e9676da8eb28a63f0ad2d52e7
i.bazhenov <ivan.bazhenov@gmail.com>
2024-04-18 14:52:14 +0700

    Added Category factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class CategoryFactory extends Factory
+{
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+        ];
+    }
+}
